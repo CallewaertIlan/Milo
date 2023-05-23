87c8d3df02b5042f1e280f843798fd8e47fd56b2
Maxadore <maxime.desnoues@orange.fr>
2023-05-23 09:37:53 +0200

    MovePlayer

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField] private CharacterController cc;
+
+    [SerializeField] private float walkSpeed = 5.0f;
+    [SerializeField] private float runSpeed = 10.0f;
+    [SerializeField] private float gravity = 20.0f;
+    [SerializeField] private Vector3 direction = Vector3.zero;
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            direction = new Vector3(Input.GetAxis("Horizontal") * runSpeed, direction.y, Input.GetAxis("Vertical") * runSpeed);
+        }
+        else
+        {
+            direction = new Vector3(Input.GetAxis("Horizontal") * walkSpeed, direction.y, Input.GetAxis("Vertical") * walkSpeed);
+        }
+
+        direction.y -= gravity * Time.deltaTime;
+
+        cc.Move(direction * Time.deltaTime);
+    }
+}
