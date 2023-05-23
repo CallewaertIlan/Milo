abdcc57341b8279de5ab0ff58fe434583e22e041
Maxadore <maxime.desnoues@orange.fr>
2023-05-23 16:12:51 +0200

    Move Player with Camera

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -4,27 +4,60 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    [SerializeField] private CharacterController cc;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float jump;
+    [SerializeField] private float gravity;
+    
+    private Rigidbody rb;
+    private Transform cameraTransform;
 
-    [SerializeField] private float walkSpeed = 5.0f;
-    [SerializeField] private float runSpeed = 10.0f;
-    [SerializeField] private float gravity = 20.0f;
-    [SerializeField] private Vector3 direction = Vector3.zero;
+    private Vector3 direction;
 
-
-    void Update()
+    private void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        cameraTransform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Vector3 cameraForward = Vector3.Scale(cameraTransform.forward, new Vector3(1, 0, 1)).normalized;
+        // Vector3 moveDirection = (cameraForward * verticalInput + cameraTransform.right * horizontalInput).normalized;
+
+        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput);
+
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            direction = new Vector3(Input.GetAxis("Horizontal") * runSpeed, direction.y, Input.GetAxis("Vertical") * runSpeed);
+            transform.Translate(cameraTransform.forward * walkSpeed * Time.deltaTime);
         }
         else
         {
-            direction = new Vector3(Input.GetAxis("Horizontal") * walkSpeed, direction.y, Input.GetAxis("Vertical") * walkSpeed);
+            transform.Translate(cameraTransform.forward * walkSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
         }
 
         direction.y -= gravity * Time.deltaTime;
 
-        cc.Move(direction * Time.deltaTime);
+        /*
+
+        if (direction.x != 0 || direction.z != 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z)), 0.015f);
+        }
+
+        */
     }
 }
