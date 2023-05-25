740bd7c337f0d6e8e36ac66873dcca11ddbfb4ff
CallewaertIlan <ilan.callewaert.pro@gmail.com>
2023-05-25 15:37:19 +0200

    create item class + boots + chestplate + helmet + pants + weapon class

---
+++ Assets/Scripts/Helmet.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helmet : Item
+{
+    protected void Start()
+    {
+        itemType = Type.HELMET;
+    }
+}
