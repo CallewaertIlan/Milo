79cbdb450bb77f1a641d3da3cb4579e12afff52c
unknown <mozanne@gamingcampus.local>
2023-06-23 19:58:21 +0200

    Created tornado visualisation prefab and class

---
+++ Assets/Scripts/TornadoSpell.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TornadoSpell : Spell
+{
+    [SerializeField] private float castDistance;
+    [SerializeField] private float tornadoRadius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
